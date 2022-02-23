bd656d71430fc2ca9e1d4e26b309e8d2f7fae372
Debasis Roy <debashish@siliconorchard.com>
2022-02-23 13:50:40 +0600

    Test Violation

--- index.php
+++ index.php
@@ -4,4 +4,14 @@ class Index {
     public function getPages() {
         return [];
     }
+
+    public function loopy() {
+        $i = 0;
+        loop:
+        echo("i = $i");
+        $i++;
+        if ($i < 10){
+            goto loop;
+        }
+    }
 }
